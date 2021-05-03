435f1be89860ccb9dbae2e96874ffa01a62d05c5
mingame86 <mingame86@gmail.com>
2021-05-04 02:52:45 +0430

    start

--- work.py
+++ work.py
@@ -1,14 +1,14 @@
 adad1 = float(input('adad '))
 alamat = (input('alamat '))
 adad2 = float(input('adad '))
-if alamat==+ :  
-    adad1+adad2
-if alamat==- :  
-    adad1-adad2
-if alamat==* :  
-    adad1*adad2
-if alamat==/ :  
-    adad1/adad2
-if alamat==^ :  
-    adad1**adad2
-
+if alamat=='+' :  
+  adad3=adad1+adad2
+if alamat=='-' :  
+  adad3=adad1-adad2
+if alamat=='*' :  
+   adad3=adad1*adad2
+if alamat=='/' :  
+    adad3=adad1/adad2
+if alamat=='^' :  
+  adad3=adad1**adad2
+prin ('javab = 'javab)
