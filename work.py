6a1f24b7821eedda988b82097bb566236a07b9f0
mingame86 <mingame86@gmail.com>
2021-05-04 14:00:09 +0430

    a

--- work.py
+++ work.py
@@ -1,6 +1,10 @@
 adad1 = float(input('adad '))
 alamat = (input('alamat '))
 adad2 = float(input('adad '))
+alamaat = ['+','-','*','/','%','^','**
+']
+if alamat!=alamaat :  
+  print (lotfan az alamat haye monaseb estefade konid.)
 if alamat=='+' :  
     adad3=adad1+adad2
 if alamat=='-' :  
